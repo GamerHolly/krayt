cd4feff11fd1f3f51bd62c4812cec3982eef72a7
Kirill <kirill98r@inbox.ru>
2020-02-10 01:29:11 +0300

    add slider

--- app/js/common.js
+++ app/js/common.js
@@ -3,4 +3,29 @@ $(document).ready(function(){
         $('.header-menu__burger, .menu-nav').toggleClass('active');
         $('body').toggleClass('lock');
     });
+    $('.slider').slick({
+        arrows: true,
+        apadtiveHeight: true,
+        slidesToShow:3,
+        centerMode: true,
+        centerPadding: '10px',
+        responsive: [
+            {
+              breakpoint: 768,
+              settings: {
+                arrows: false,
+                dots:true,
+                slidesToShow: 3
+              }
+            },
+            {
+              breakpoint: 480,
+              settings: {
+                arrows: false,
+                dots:true,
+                slidesToShow: 1
+              }
+            }
+          ]
+    });
 });
